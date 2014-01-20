680b7a2cc99ece4797f5a1a2db1b7c11cb6ce99c
Chandan Kumar <chandan.gupta@gmail.com>
2014-01-20 07:50:30 +0530

    Added palindrome functions

--- a.erl
+++ a.erl
@@ -1,15 +1,32 @@
 -module(a).
 -compile(export_all).
 
+%
+% Sum of numbers in a list
+%
 sum([]) -> 0;
 sum([H | T]) -> H + sum(T).
+%
+% Reverses a string
+%
+reverse([]) -> [];
+reverse([H | T]) ->
+    reverse2([H | T], []).
 
-area2({rectangle, W, H}) -> W*H;
-area2({square, S}) -> S*S;
-area2(Any1) ->
+reverse2([], C) -> C;
+reverse2([H | T], C) ->
+    reverse2(T, [H | C]).
+%
+% Computes area
+%
+area({rectangle, W, H}) -> W*H;
+area({square, S}) -> S*S;
+area(Any1) ->
     io:format("Don't know how to calculate area of ~p~n", [Any1]),
-    ok.
-
+    error.
+%
+% Sorts a list of numbers.
+%
 sort([]) -> [];
 sort([H]) -> [H];
 sort([H | T]) ->
@@ -45,3 +62,47 @@ greater_than2(H, [H1 | T1], C) ->
             greater_than2(H, T1, C)
     end.
 %
+% Reads two numbers and prints their sum.
+% Issue: THIS IS NOT WORKING :(
+%
+two_sum() ->
+    {ok, A} = io:fread("Enter first number>", "~d"),
+    io:format("You entered ~p~n", A),
+    {ok, B} = io:fread("Enter second number>", "~d"),
+    io:format("You entered ~p~n", B),
+    C = A + B,
+    io:format("Sum of the two numbers: ~p~n", C).
+%
+% Returns true if provided string is a palindrome.
+% Issue: Raises "** exception error: bad argument" for floats
+%
+palindrome([]) -> true;
+palindrome([_H]) -> true;
+palindrome([H | T]) ->
+    L = [H | T],
+    MID = length(L),
+    if
+        MID rem 2 == 0 ->
+            MID1 = MID div 2;
+        true ->
+            MID1 = (MID div 2) + 1
+    end,
+    SECOND_HALF = lists:nthtail(MID1, L),
+    lists:prefix(lists:reverse(SECOND_HALF), L);
+palindrome(N) ->
+    palindrome(integer_to_list(N)).
+%
+% Generates the smallest palindrome number greater than the input number
+%
+next_palin(N) ->
+    next_palin2(N, false).
+
+next_palin2(N, B) ->
+    if
+        B ->
+            N;
+        true ->
+            next_palin2(N+1, palindrome(N+1))
+    end.
+
+    
